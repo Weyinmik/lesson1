2fe7ae05c6b7d710cc14aff033ffe7963f31bca3
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-05 12:16:25 +0100

    more method added and Test Suite created

---
+++ src/AllTests.java
@@ -0,0 +1,10 @@
+import junit.framework.TestSuite;
+
+public class AllTests {
+    public static TestSuite suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTestSuite(StudentTest.class);
+        suite.addTestSuite(CourseSessionTest.class);
+        return suite;
+    }
+}
--- src/CourseSessionTest.java
+++ src/CourseSessionTest.java
@@ -3,5 +3,17 @@ public class CourseSessionTest extends junit.framework.TestCase {
         CourseSession session = new CourseSession("ENGL", "101");
         assertEquals("ENGL", session.getDepartment());
         assertEquals("101", session.getNumber());
+        assertEquals(0, session.getNumberOfStudents());
+    }
+
+    public void testEnrollStudents() {
+        CourseSession session = new CourseSession("ENGL", "101");
+        Student student1 = new Student("Esther Kol");
+        session.enroll(student1);
+        assertEquals(1, session.getNumberOfStudents());
+
+        Student student2 = new Student("Benny Hil");
+        session.enroll(student2);
+        assertEquals(2, session.getNumberOfStudents());
     }
 }
